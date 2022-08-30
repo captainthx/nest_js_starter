2ceb15f0e688367b3b0a8bc1cd7f2a28426662cc
captainthx <106069231+captainthx@users.noreply.github.com>
2022-08-30 17:38:42 +0700

    [Add]All

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000);
+  console.log('PORT', process.env.PORT || 3000);
+}
+bootstrap();
